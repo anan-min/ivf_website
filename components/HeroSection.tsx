710425fc051a65b95f3b352e8edd545d29211c59
nut <nut999anan@hotmail.com>
2025-02-12 18:49:33 +0700

    hero section done

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+"use client";
+import { cn } from "@/lib/utils";
+import { GridPattern } from "@/components/magicui/grid-pattern";
+import React from "react";
+import { Button } from "./ui/button";
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
-  )
-}
+    <div className="bg-[#ffe6db] min-h-[60vh] lg:min-h-[70vh] min-w-full relative mt-16 p-16">
+      {/* Grid pattern as a background */}
+      <div className="absolute inset-0">
+        <GridPattern
+          width={20}
+          height={20}
+          x={-1}
+          y={-1}
+          className={cn(
+            "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] fill-black"
+          )}
+        />
+      </div>
 
-export default HeroSection
+      {/* Main content */}
+      <main className="relative z-10 flex flex-col w-10/12 items-center justify-center mx-auto text-center space-y-6 px-4 sm:px-8">
+        <h1 className="text-5xl sm:text-5xl font-bold lg:mt-24">
+          Plan Your{" "}
+          <span className="bg-[#fcd8c9] rounded-xl px-4 py-1/2 my-2 text-nowrap">
+            IVF Journey
+          </span>{" "}
+          with Confidence
+        </h1>
+        <p className="text-lg sm:text-2xl text-gray-700">
+          Take the stress out of your IVF planning. Select your first date and
+          receive a personalized timeline, event details, and pricing tailored
+          to your needs.
+        </p>
+        <div className="flex flex-row space-x-4">
+          <Button className="px-6 py-3 bg-white hover:bg-[#e9e3e0]  text-stone-7000 font-medium">
+            Learn More
+          </Button>
+          <Button className="text-grey-700 px-6 py-3 bg-[#ffc1b5] hover:bg-[#ffd5cd]  font-medium">
+            Start Planning
+          </Button>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default HeroSection;
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,51 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a
+          href="https://flowbite.com/"
+          className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+          <span className="self-center text-2xl font-semibold text-[#daa9a9] whitespace-nowrap dark:text-white pl-2">
+            Bloom
+          </span>
+        </a>
+        <div
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-sticky"
+        >
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:p-0 hover:text-[#ffb8aa]"
+              >
+                Infos
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:p-0 hover:text-[#ffb8aa]"
+              >
+                Plannings
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:p-0 hover:text-[#ffb8aa]"
+              >
+                Packages
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
